bdec43e7f786780a4b2f6011b8864e9ccd74f5b1
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-01 16:30:43 -0600

    JS done, updating visuals

--- script.js
+++ script.js
@@ -1,54 +1,88 @@
 const clicker = document.querySelector('#clicker');
 const DOMScore = document.querySelector('#score');
-let queryResources = document.querySelectorAll('.sidebarEntry');
+let queryProducers = document.querySelectorAll('.sidebarEntry');
 let resource = 0;
+let scaling = 2.5; // How much the cost goes up after each buy, 2.5 seems good
 
 // Amount resources will raise score per second
-resourceQuantities = {
-  resource1: 1,
-  resource2: 5,
-  resource3: 20,
+const producers = {
+  producer1: {
+      yield: 1,
+      bought: 0,
+      cost: 1
+  },
+  producer2: {
+      yield: 5,
+      bought: 0,
+      cost: 40
+  },
+  producer3: {
+    yield: 20,
+    bought: 0,
+    cost: 200
+  } 
 }
 
-resourcesBought = {
-  resource1: 0,
-  resource2: 0,
-  resource3: 0,
+// Map IDs to resources bought ID so we don't query over and over
+for (key in producers) {
+  let producer = producers[key];
+
+  boughtID = key + 'Bought';
+  const score = document.querySelector(`#${key} .producerScore`);
+  
+  const cost = document.querySelector(`#${key} .cost`);
+
+  producer.scoreElement = score;
+  producer.costElement = cost;
 }
+console.log(producers)
 
-// Map IDs to resources bought ID so we don't quert over and over
-resourceIDtoBought = {};
-for (let i = 0; i < queryResources.length; i++) {
-  idOfSelector = queryResources[i].id;
-
-  boughtID = idOfSelector + 'Bought';
-  const score = document.querySelector(`#${boughtID}`);
-
-  resourceIDtoBought[idOfSelector] = score;
-}
-console.log(resourceIDtoBought)
-
-
-
-
-// Add functionality
-function updateScore() {
-  resource = resource + 1;
+function updateDomScore() {
   DOMScore.textContent = `Score: ${resource}`;
 }
 
-clicker.addEventListener('click', updateScore);
+
+// Add event listeners
+function updateScoreOnClick() {
+  resource = resource + 1;
+  updateDomScore();
+}
+
+clicker.addEventListener('click', updateScoreOnClick);
+
 
 function updateResource(evt) {
-  resourceID = evt.target.id;
+  let resourceID = evt.currentTarget.id;
+  let producer = producers[resourceID];
 
-  resourcesBought[resourceID] += 1;
-  console.log('event target', evt.target)
-  console.log('ID', resourceID)
-  console.log('object', resourceIDtoBought)
-  resourceIDtoBought[resourceID].textContent = resourcesBought[resourceID];
+  if (resource >= producer.cost) {
+    resource = resource - producer.cost;
+    updateDomScore();
+
+    producer.cost = Math.round(scaling * producer.cost,);
+    producer.costElement.textContent = `Cost: ${producer.cost}`;
+    
+    producer.bought += 1;
+    producer.scoreElement.textContent = producer.bought;
+  }
 }
 
-for (let i = 0; i < queryResources.length; i++) {
-  queryResources[i].addEventListener('click', updateResource);
+for (let i = 0; i < queryProducers.length; i++) {
+  queryProducers[i].addEventListener('click', updateResource);
 }
+
+
+function updateResourceFromProducers() {
+  let resourcesToAdd = 0;
+  for (key in producers) {
+    const producer = producers[key];
+
+    amountToAdd = producer.yield * producer.bought;
+    resourcesToAdd += amountToAdd;
+  }
+
+  resource += resourcesToAdd;
+  updateDomScore();
+}
+
+setInterval(updateResourceFromProducers, 1000);
